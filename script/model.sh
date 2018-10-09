87d0c39ed69f5508bf002b863207944fd4a5fa67
Glenn <glennjgalvizo@gmail.com>
2018-10-08 21:33:11 -1000

    Script for model MCMC now uses the new population model and the new parameter names.

--- script/model.sh
+++ script/model.sh
@@ -5,30 +5,27 @@ for run in 1 2 3; do
 
     # Our criteria for the loci and sample IDs to use for this run of MCMC.
     sample_uids=(); loci=()
-    for r in $(sqlite3 data/real.db "SELECT DISTINCT SAMPLE_UID, LOCUS FROM REAL_ELL"); do
+    for r in $(sqlite3 data/real.db "SELECT DISTINCT SAMPLE_UID, LOCUS FROM OBSERVED_ELL"); do
         IFS='|' read -r -a array <<< "$r"
         sample_uids+="${array[0]} "; loci+="${array[1]} "
     done
 
     # Run the MCMC. We are now only focusing on PL1 model from Sainudiin paper.
     python3 src/model.py \
-        -edb "data/model-${run}-abc.db" \
-        -type "ABC" \
-        -rs 200 \
-        -rp 10 \
+        -mdb "data/model-${run}-abc.db" \
+        -simulation_n 200 \
+        -sample_n 10 \
         -epsilon 0.65 \
-        -it 25000 \
-        -rsu ${sample_uids} \
-        -l ${loci} \
-        -big_n 1000 -big_n_sigma 0.0 \
-        -mu 0.001 -mu_sigma 0.01 \
-        -s 0.05 -s_sigma 0.01 \
+        -iterations_n 25000 \
+        -uid_observed ${sample_uids} \
+        -locus_observed ${loci} \
+        -n 1000 -n_sigma 0.0 \
+        -f 1 -f_sigma 0.0 \
+        -c 0.001 -c_sigma 0.001 \
+        -u 1.2 -u_sigma 0.001 \
+        -d 0.001 -d_sigma 0.001 \
         -kappa 3 -kappa_sigma 0.0 \
-        -omega 100 -omega_sigma 0.0 \
-        -u 0.90 -u_sigma 0.01 \
-        -v 0.03 -v_sigma 0.01 \
-        -m 1 -m_sigma 0.0 \
-        -p 0.0 -p_sigma 0.0
+        -omega 100 -omega_sigma 0.0
 
 done
 echo "MCMC is finished!"
