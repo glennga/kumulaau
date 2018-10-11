12327e2ca59e7832622c890e3ca1c3d244634678
Glenn <glennjgalvizo@gmail.com>
2018-10-11 10:53:22 -1000

    Removed the compare and exhaust scripts.

--- src/waiting.py
+++ src/waiting.py
@@ -9,44 +9,42 @@ def set_style() -> None:
     """
     from matplotlib import rc
 
-    plt.style.use('bmh')  # Change that ugly default matplotlib look.
-    rc('text', usetex=True), plt.rc('font', family='serif')  # Use TeX.
-
+    # Change that ugly default matplotlib look and use TeX.
+    plt.style.use('bmh')
+    rc('text', usetex=True), plt.rc('font', family='serif')
     plt.figure(figsize=(13, 7), dpi=100)
 
 
-def model(cur_j: Cursor, hs: Dict[str, float]) -> None:
-    """ Display the results of the MCMC script for our mutation model: a histogram of each parameter.
+def histogram_1(cursor: Cursor, step_sizes: Dict[str, float]) -> None:
+    """ Display the results of the MCMC script for our mutation model: a histogram of waiting times for each parameter.
 
-    :param cur_j: Cursor to the MCMC database to pull the data from.
-    :param hs: Histogram step sizes for MU, S, U, V, M, and P dimensions.
+    :param cursor: Cursor to the MCMC database to pull the data from.
+    :param step_sizes: Histogram step sizes for the N, F, C, U, D, KAPPA, OMEGA dimensions.
     :return: None.
     """
-    from scipy.stats import beta
-    from numpy import arange, linspace
     from itertools import chain
+    from numpy import arange
 
-    set_style(), plt.suptitle(r'Posterior Distribution Samples (MCMC)')
+    set_style(), plt.suptitle(r'Waiting Times for Mutation Model MCMC')
 
-    dimension_labels = [r'$N$', r'$\mu$', r'$s$', r'$K$', r'$\Omega$', r'$u$', r'$v$', r'$m$', r'$p$']
-    for j, dimension in enumerate(['BIG_N', 'MU', 'S', 'KAPPA', 'OMEGA', 'U', 'V', 'M', 'P']):
-        plt.subplot(3, 3, j + 1)
+    dimension_labels = [r'$N$', r'$f$', r'$c$', r'$u$', r'$d$', r'$\kappa$', r'$\Omega$']
+    for j, dimension in enumerate(['N', 'F', 'C', 'U', 'D', 'KAPPA', 'OMEGA']):
+        plt.subplot(2, 4, j + 1)
 
         # Grab the minimum and maximum values for the current dimension.
-        min_dimension, max_dimension = [float(x) for x in cur_j.execute(f"""
+        min_dimension, max_dimension = [float(x) for x in cursor.execute(f"""
           SELECT MIN(CAST({dimension} AS FLOAT)), MAX(CAST({dimension} AS FLOAT))
           FROM WAIT_MODEL
         """).fetchone()]
 
-        # Our bin widths depend on the current dimension.
-        if min_dimension != max_dimension:
-            bins = arange(min_dimension, max_dimension, (hs[dimension] if dimension not in
-                                                         ['BIG_N', 'KAPPA', 'OMEGA'] else 1))
+        if min_dimension != max_dimension:  # Our bin widths depend on the current dimension.
+            bins = arange(min_dimension, max_dimension,
+                          (step_sizes[dimension] if dimension not in ['BIG_N', 'KAPPA', 'OMEGA'] else 1))
         else:
             bins = 'auto'
 
         # Obtain the data to plot. Waiting times indicate the number of repeats to apply to this state.
-        axis_wait = cur_j.execute(f"""
+        axis_wait = cursor.execute(f"""
           SELECT CAST({dimension} AS FLOAT), WAITING
           FROM WAIT_MODEL
         """).fetchall()
@@ -54,10 +52,6 @@ def model(cur_j: Cursor, hs: Dict[str, float]) -> None:
 
         # Plot the histogram, and find the best fit line (assuming beta distribution).
         plt.gca().set_title(dimension_labels[j]), plt.hist(axis, bins=bins, density=True, histtype='stepfilled')
-        if axis.count(axis[0]) != len(axis):  # Do not plot for constant dimensions.
-            spc, c_ylim = linspace(min(plt.xticks()[0]), max(plt.xticks()[0]), len(axis)), plt.ylim()
-            ab, bb, cb, db = beta.fit(axis)
-            plt.plot(spc, beta.pdf(spc, ab, bb, cb, db)), plt.ylim(c_ylim)  # Retain past y-limits, focus is histogram.
 
     plt.subplots_adjust(top=0.924, bottom=0.051, left=0.032, right=0.983, hspace=0.432, wspace=0.135)
 
@@ -67,13 +61,19 @@ if __name__ == '__main__':
     from sqlite3 import connect
     from matplotlib import use
 
-    parser = ArgumentParser(description='Display the results of various MCMC scripts.')
-    parser.add_argument('-db', help='Location of the database required to operate on.', type=str)
-    parser.add_argument('-f', help='Data to display.', type=str, choices=['model'])
-    paa = lambda paa_1, paa_2, paa_3: parser.add_argument(paa_1, help=paa_2, type=paa_3)
+    # Generate our plot help strings.
+    plot_help = """ Visualization function to use: 
+        [1 <- Waiting times histogram of mutation model MCMC.]
+    """
+    plot_parmeters_help = """ Parameters associated with function of use:
+        [1 <- Step sizes of histogram in following order: N, F, C, U, D, KAPPA, OMEGA.] 
+    """
 
-    paa('-image', 'Image file to save resulting repeat length distribution (histogram) to.', str)
-    parser.add_argument('-hs', help='(posterior) Histogram step sizes in order: MU, S, U, V, M, P', nargs=6, type=float)
+    parser = ArgumentParser(description='Display the results of MCMC scripts.')
+    parser.add_argument('-db', help='Location of the database required to operate on.', type=str)
+    parser.add_argument('-plot', help=plot_help, type=int, choices=[1])
+    parser.add_argument('-image_file', help='Image file to save resulting figure to.', type=str)
+    parser.add_argument('plot_parameters', help='Parameters associated with function of use.', type=float, nargs='+')
     main_arguments = parser.parse_args()  # Parse our arguments.
 
     # Connect to the appropriate database.
@@ -81,12 +81,14 @@ if __name__ == '__main__':
     cursor_r = connection_r.cursor()
 
     # Determine if we need to use X-server (plotting to display).
-    use('Agg') if main_arguments.image is not None else None
+    use('Agg') if main_arguments.image_file is not None else None
     from matplotlib import pyplot as plt
 
     # Perform the appropriate function.
-    if main_arguments.f == 'model':
-        model(cursor_r, dict(zip(['MU', 'S', 'U', 'V', 'M', 'P'], main_arguments.hs)))
+    if main_arguments.plot == 1 and len(main_arguments.plot_parameters) == 7:
+        histogram_1(cursor_r, dict(zip(['N', 'F', 'C', 'U', 'D', 'KAPPA', 'OMEGA'], main_arguments.plot_parameters)))
+    else:
+        print('Incorrect number of plot parameters.') and exit(-1)
 
     # Save or display, your choice!
-    plt.savefig(main_arguments.image, dpi=100) if main_arguments.image is not None else plt.show()
+    plt.savefig(main_arguments.image_file, dpi=100) if main_arguments.image_file is not None else plt.show()
