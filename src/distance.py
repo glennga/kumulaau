8a2c0e23b817e528fe0537bc9b5627aaee3faf81
Glenn <glennjgalvizo@gmail.com>
2018-10-26 11:47:20 -1000

    A distance instance now keeps track of the number of times a given distance falls below a given threshold. This allows us to compute the likelihood of an approximate distribution. Cosine class now refers to the angular distance between two vectors (a proper distance measure) as opposed to the cosine distance itself. Added Eucledian distance as well, not tested but I'm pretty positive it works.

--- src/distance.py
+++ src/distance.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 from sqlite3 import Cursor
 from typing import List
-from numpy.linalg import norm
-from numpy import ndarray, dot
-from numba import jit, prange
 from abc import ABC, abstractmethod
+from numpy.linalg import norm
+from numpy import ndarray, dot, arccos, pi
+from numba import jit
 
 
 def create_table(cursor: Cursor) -> None:
@@ -14,7 +14,7 @@ def create_table(cursor: Cursor) -> None:
     :return: None.
     """
     cursor.execute("""
-        CREATE TABLE IF NOT EXISTS SUMMARY_POP (
+        CREATE TABLE IF NOT EXISTS DISTANCE_POP (
             TIME_R TIMESTAMP,
             ID_GENERATED TEXT,
             UID_OBSERVED TEXT,
@@ -39,143 +39,175 @@ def log_distances(cursor: Cursor, distances: ndarray, uid_observed: str, locus_o
 
     # Record our results.
     cursor.executemany("""
-        INSERT INTO SUMMARY_POP
+        INSERT INTO DISTANCE_POP
         VALUES (?, ?, ?, ?, ?)
     """, ((datetime.now(), ''.join(choice(ascii_uppercase + digits) for _ in range(20)), uid_observed,
            locus_observed, a) for a in distances))
 
 
-class Summary(ABC):
-    def __init__(self, generated_sample: ndarray=None):
+class Distance(ABC):
+    def __init__(self, observed_frequencies_dirty: List, kappa: int, omega: int, simulation_n: int,
+                 generated_sample: ndarray=None):
         """ Constructor. Store the population we are comparing to.
 
-        :param generated_sample: Simulated population of repeat units (one dimensional list). Optional.
+        :param observed_frequencies_dirty: Dirty observed frequency samples. Needs to be transformed into sparse vector.
+        :param kappa: Lower bound of our state space. omega - kappa = vector dimensionality to extract distance from.
+        :param omega: Upper bound of our state space. omega - kappa = vector dimensionality to extract distance from.
+        :param simulation_n: Number of simulations to run per generated population (number of rows for matched matrix).
+        :param generated_sample: Simulated population of repeat units (one dimensional list). Specify now or later.
         """
         from numpy import array
-        self.generated_sample = generated_sample
+
+        # Save our parameters.
+        self.generated_sample, self.omega, self.kappa = generated_sample, omega, kappa
 
         # We will set the following fields upon preparation.
-        self.generated_frequency, self.observed_frequency, self.distances = [array([]) for _ in range(3)]
+        self.generated_frequency, self.observed_frequencies, self.matched_matrix = [array([]) for _ in range(3)]
 
-    def _prepare(self, observed_frequency_dirty: List) -> None:
-        """ Given a "dirty" (non-length normalized) frequency vector of observed samples, generate the appropriate
-        vectors to store to.
+        # Generate the match matrix and clean the observation set.
+        self._prepare(observed_frequencies_dirty, simulation_n)
 
-        :param observed_frequency_dirty: Dirty observed frequency sample. Needs to be transformed into a sparse vector.
-        :return: None.
+    def _prepare(self, observed_frequencies_dirty: List, simulation_n: int) -> None:
+        """ TODO
+
+        :param observed_frequencies_dirty:
+        :param simulation_n:
+        :return:
         """
         from numpy import zeros
 
         # Cast our observed frequencies into numbers.
-        observed_dictionary = {int(a[0]): float(a[1]) for a in observed_frequency_dirty}
-
-        # Determine the omega and kappa from the simulated population and our observed sample.
-        self.omega = max(self.generated_sample) + 1 if \
-            max(self.generated_sample) > max(observed_dictionary.keys()) else max(observed_dictionary.keys()) + 1
-        self.kappa = min(self.generated_sample) if \
-            min(self.generated_sample) < min(observed_dictionary.keys()) else min(observed_dictionary.keys())
-
-        # Create the vectors to return.
-        self.generated_frequency, self.observed_frequency, self.distances = \
-            [zeros(self.omega), zeros(self.omega), zeros(1)]  # TODO: Convert self.distances into not a list...
+        observation_dictionary = [{int(a[0]): float(a[1]) for a in b} for b in observed_frequencies_dirty]
 
         # Fit our observed distribution into a sparse frequency vector.
-        for repeat_unit in observed_dictionary.keys():
-            self.observed_frequency[repeat_unit] = observed_dictionary[repeat_unit]
+        self.observed_frequencies = [zeros(self.omega - self.kappa + 1) for _ in observed_frequencies_dirty]
+        for j, observation in enumerate(observation_dictionary):
+            for repeat_unit in observation.keys():
+                self.observed_frequencies[j][repeat_unit - self.kappa + 1] = observation[repeat_unit]
+
+        # Prepare the storage vector for our generated frequency vector.
+        self.generated_frequency = zeros(self.omega - self.kappa + 1)
+
+        # Construct an empty matched matrix.
+        self.matched_matrix = zeros((simulation_n, len(observed_frequencies_dirty)))
 
     @staticmethod
     @abstractmethod
-    def _distance(generated_sample: ndarray, generated_frequency: ndarray, observed_frequency: ndarray,
-                  distances: ndarray, omega: int, kappa: int) -> None:
+    def _delta(generated_sample: ndarray, generated_frequency: ndarray, observed_frequency: ndarray,
+               omega: int, kappa: int) -> float:
         """ Given individuals from the simulated population and the frequencies of individuals from an observed sample,
-        sample the same amount from the simulated population 'r' times and determine the differences in distribution
-        for each different simulated sample. All vectors passed MUST be of appropriate size and must be zeroed out
-        before use. Optimized by Numba.
+        determine the differences in distribution for each different simulated sample. All vectors passed MUST be of
+        appropriate size and must be zeroed out before use. Optimized by Numba.
 
         :param generated_sample: Storage vector, used to hold the sampled simulated population.
         :param generated_frequency: Storage sparse vector, used to hold the frequency sample.
         :param observed_frequency: Observed frequency sample as a sparse frequency vector indexed by repeat length.
-        :param distances: Output vector, used to store the computed distances of each sample.
         :param omega: Upper bound of the repeat unit space.
         :param kappa: Lower bound of the repeat unit space.
+        :return: The distance between the generated and observed population.
         """
         raise NotImplementedError
 
-    def compute_distance(self, observed_frequency_dirty: List, generated_sample: ndarray=None) -> ndarray:
-        """ Compute the distance for 'sample_n' samples, and return the results. For repeated runs of this function,
-        we append our results to the same 'distances' field.
+    def _compute_delta(self, observed_frequency: ndarray, generated_sample: ndarray) -> float:
+        """ Compute the distance between a generated sample and an observation.
 
-        :param observed_frequency_dirty: Dirty observed frequency sample. Needs to be transformed into a sparse vector.
+        :param observed_frequency: Observed frequency sample as a sparse frequency vector indexed by repeat length.
         :param generated_sample: Simulated population of repeat units (one dimensional list). Optional.
-        :return: The computed distances for each sample.
+        :return: The computed distance.
         """
-        from numpy import concatenate, array
-        self.generated_sample = generated_sample if generated_sample is not None else array([])
+        return self._delta(generated_sample, self.generated_frequency, observed_frequency, self.omega, self.kappa)
 
-        d_previous = array(self.distances)  # Save our previous state.
-        self._prepare(observed_frequency_dirty)
+    def compute_deltas(self, epsilon: float, generated_number: int, generated_sample: ndarray=None) -> ndarray:
+        """ Compute the distance between a set of observed frequencies and some generated sample. If a given
+        distance falls below epsilon, we mark this as a match (1 in matched_matrix). Otherwise, it gets marked as a 0.
 
-        # Run our sampling and comparison. Concatenate our previous state.
-        self._distance(self.generated_sample, self.generated_frequency, self.observed_frequency,
-                       self.distances, self.omega, self.kappa)
-        self.distances = concatenate((self.distances, d_previous), axis=None)
-
-        return self.distances
-
-    def compute_distance_multiple(self, observed_frequencies_dirty: List, generated_sample: ndarray=None) -> None:
-        """ TODO: Finish documentation for compute_distance_multiple.
-
-        :param observed_frequencies_dirty: Dirty observed frequency samples.
+        :param epsilon: The minimum distance between frequencies to label as a match.
+        :param generated_number: The current generated population we are working with. Indicates the row to access.
         :param generated_sample: Simulated population of repeat units (one dimensional list). Optional.
-        :return:
+        :return: The vector of distances from each observed frequency to the generated frequency.
         """
-        for a in observed_frequencies_dirty:
-            self.compute_distance(a, generated_sample)
+        from numpy import zeros
 
-    def average_distance(self):
-        """ TODO: Finish documentation for average_distance.
+        # For each of observed frequencies, we compute a distance.
+        self.generated_sample = generated_sample if generated_sample is not None else self.generated_sample
+        deltas_at_number = zeros(len(self.observed_frequencies))
 
-        :return:
+        # If our distance is less than a defined epsilon, we mark this as 'matched' with 1. Otherwise, 0.
+        for j, observed_frequency in enumerate(self.observed_frequencies):
+            deltas_at_number[j] = self._compute_delta(observed_frequency, self.generated_sample)
+            self.matched_matrix[generated_number, j] = 1 if deltas_at_number[j] < epsilon else 0
+
+        # Return our distance vector.
+        return deltas_at_number
+
+    def match_likelihood(self) -> ndarray:
+        """ 'j' specifies the column or associated observed microsatellite sample in 'matched_matrix'. To determine
+        the probability of a model (parameters) matching this observed sample, we compute the average of this column.
+        Repeat this for all 'j's, and take the product (assumes each is independent).
+
+        :return: The likelihood the generated sample set matches our observations.
         """
-        from numpy import average
-        return average(self.distances)
+        from numpy import mean, log, exp
+
+        # Avoid floating point error, use logarithms.
+        return exp(sum(map(log, mean(self.matched_matrix, axis=0))))
 
 
-class Cosine(Summary):
-    # If our distance function returns a 0, then we have found a orthogonal vector. This is the "worst case" scenario.
-    COMPLETE_DIFFERENCE = 0
-
-    # If our distance function returns a 1, then we have found an exact match.
-    COMPLETE_MATCH = 1
-
+class Cosine(Distance):
     @staticmethod
     @jit(nopython=True, nogil=True, target='cpu', parallel=True)
-    def _distance(generated_sample: ndarray, generated_frequency: ndarray, observed_frequency: ndarray,
-                  distances: ndarray, omega: int, kappa: int) -> None:
+    def _delta(generated_sample: ndarray, generated_frequency: ndarray, observed_frequency: ndarray,
+               omega: int, kappa: int) -> float:
         """ Given individuals from the simulated population and the frequencies of individuals from an observed sample,
-        sample the same amount from the simulated population 'r' times and determine the differences in distribution
-        for each different simulated sample. All vectors passed MUST be of appropriate size and must be zeroed out
-         before use. Optimized by Numba.
+        determine the differences in distribution for each different simulated sample. All vectors passed MUST be of
+        appropriate size and must be zeroed out before use. In order to transform this into a proper distance, we
+        compute the angular cosine distance. We assume both vectors are always positive. Optimized by Numba.
 
         :param generated_sample: Storage vector, used to hold the sampled simulated population.
         :param generated_frequency: Storage sparse vector, used to hold the frequency sample.
         :param observed_frequency: Observed frequency sample as a sparse frequency vector indexed by repeat length.
-        :param distances: Output vector, used to store the computed distances of each sample.
         :param omega: Upper bound of the repeat unit space.
         :param kappa: Lower bound of the repeat unit space.
-        :return: None.
+        :return: The distance between the generated and observed population.
         """
-        for k_0 in prange(distances.size):
-            # Fit the simulated population into a sparse vector of frequencies.
-            for repeat_unit in prange(kappa, omega):
-                i_count = 0
-                for i in generated_sample:  # Ugly code, but I'm trying to avoid memory allocation. ):
-                    i_count += 1 if i == repeat_unit else 0
-                generated_frequency[repeat_unit] = i_count / generated_sample.size
+        # Fit the simulated population into a sparse vector of frequencies.
+        for repeat_unit in range(kappa, omega + 1):
+            i_count = 0
+            for i in generated_sample:  # Ugly code, but I'm trying to avoid dynamic memory allocation. ):
+                i_count += 1 if i == repeat_unit else 0
+            generated_frequency[repeat_unit - kappa] = i_count / float(generated_sample.size)
 
-            # For all repeat lengths, determine the sum difference in frequencies. Normalize this to [0, 1].
-            distances[k_0] = (dot(generated_frequency, observed_frequency) /
-                              (norm(generated_frequency) * norm(observed_frequency)))
+        # Determine the angular distance. 0 = identical, 1 = maximally dissimilar.
+        return 2.0 * arccos(dot(generated_frequency, observed_frequency) /
+                            (norm(generated_frequency) * norm(observed_frequency))) / pi
+
+
+class Euclidean(Distance):  # TODO: Actually test this metric...
+    @staticmethod
+    @jit(nopython=True, nogil=True, target='cpu', parallel=True)
+    def _delta(generated_sample: ndarray, generated_frequency: ndarray, observed_frequency: ndarray,
+               omega: int, kappa: int) -> float:
+        """ Given individuals from the simulated population and the frequencies of individuals from an observed sample,
+        determine the differences in distribution for each different simulated sample. All vectors passed MUST be of
+        appropriate size and must be zeroed out before use. Treating each distribution as a point, we compute the
+        Euclidean distance between both points. Optimized by Numba.
+
+        :param generated_sample: Storage vector, used to hold the sampled simulated population.
+        :param generated_frequency: Storage sparse vector, used to hold the frequency sample.
+        :param observed_frequency: Observed frequency sample as a sparse frequency vector indexed by repeat length.
+        :param omega: Upper bound of the repeat unit space.
+        :param kappa: Lower bound of the repeat unit space.
+        :return: The distance between the generated and observed population.
+        """
+        # Fit the simulated population into a sparse vector of frequencies.
+        for repeat_unit in range(kappa, omega + 1):
+            i_count = 0
+            for i in generated_sample:  # Ugly code, but I'm trying to avoid dynamic memory allocation. ):
+                i_count += 1 if i == repeat_unit else 0
+            generated_frequency[repeat_unit - kappa] = i_count / float(generated_sample.size)
+
+        # Determine the Euclidean distance. 0 = identical, 1 = maximally dissimilar.
+        return norm(generated_frequency - observed_frequency)
 
 
 if __name__ == '__main__':
@@ -186,7 +218,7 @@ if __name__ == '__main__':
 
     parser = ArgumentParser(description='Sample a simulated population and compare this to an observed data set.')
     parser.add_argument('-odb', help='Location of the observed database file.', type=str, default='data/observed.db')
-    parser.add_argument('-rdb', help='Location of the database to record data to.', type=str, default='data/summary.db')
+    parser.add_argument('-rdb', help='Location of the database to record data to.', type=str, default='data/delta.db')
     paa = lambda paa_1, paa_2, paa_3: parser.add_argument(paa_1, help=paa_2, type=paa_3)
 
     parser.add_argument('-function', help='Similarity function to use.', type=str, choices=['COSINE'])
@@ -208,10 +240,10 @@ if __name__ == '__main__':
 
     # Generate some population.
     main_population = Population(BaseParameters(n=100, f=1.0, c=0.01, u=1.2,
-                                                d=0.001, kappa=3, omega=100)).evolve(array([11]))
+                                                d=0.001, kappa=3, omega=30)).evolve(array([11]))
 
     # Execute the sampling.
-    main_distances = Cosine(main_population).compute_distance(main_observed_frequency)
+    main_distances = Cosine([main_observed_frequency], 3, 30, 1).compute_deltas(1.0, 0, main_population)
 
     # Display our results to console, and record to our simulated database.
     print('Result: [\n\t' + ', '.join(str(a) for a in main_distances) + '\n]')
