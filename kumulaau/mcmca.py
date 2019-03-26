e1e4795ae2de75cd77b11c970611c0a2c2f37f26
Glenn <gjgalvizo@gmail.com>
2019-03-25 16:45:58 -1000

    Separating the ABC from the distance module.
    This module is now only responsible for generating the D matrix, O is implied. This avoids having us pass around state through namespaces like savages. All ABC specific functions have been placed in mcmca.

--- kumulaau/mcmca.py
+++ kumulaau/mcmca.py
@@ -1,10 +1,39 @@
 #!/usr/bin/env python3
-from kumulaau.distance import generate_hdo, populate_hd, likelihood_from_h
+from kumulaau.distance import populate_d
 from typing import Callable, Sequence
+from numpy import ndarray
 
 
 # The model SQL associated with model database.
 SQL = 'TIME_R TIMESTAMP, WAITING_TIME INT, P_PROPOSED FLOAT, EXPECTED_DELTA FLOAT, PROPOSED_TIME INT'
+
+
+def _populate_h(h: ndarray, d: ndarray, epsilon: float) -> None:
+    """ If the distance between a observation and generated sample falls below epsilon, we count this as a matched
+    (marked 1 in the matrix H). Otherwise, we count this as a zero. This is the ABC portion.
+
+    :param h: Binary match matrix of generated x observation to populate. Must be the same size as D.
+    :param d: **Populated** D matrix, holding all distances between a generated and observed population.
+    :param epsilon: The minimum distance between frequencies to label as a match.
+    :return: None.
+    """
+    for i in range(d.shape[0]):
+        for j in range(d.shape[1]):
+            h[i, j] = 1 if d[i, j] < epsilon else 0
+
+
+def _likelihood_from_h(h: ndarray) -> float:
+    """ 'j' specifies the column or associated observed microsatellite sample in the matched matrix. To determine
+    the probability of a model (parameters) matching this observed sample, we compute the average of this column.
+    Repeat this for all 'j's, and take the product (assumes each is independent).
+
+    :param h: Matrix of instances where a observation - generated match and don't (matched matrix).
+    :return: The likelihood the generated sample set matches our observations.
+    """
+    from numpy import log, exp, mean
+
+    # Avoid floating point error, use logarithms. Avoid log(0) errors.
+    return exp(sum(map(lambda a: 0 if a == 0 else log(a), mean(h, axis=0))))
 
 
 def run(walk: Callable, sample: Callable, delta: Callable, log_handler: Callable, theta_0, observed: Sequence,
@@ -14,7 +43,7 @@ def run(walk: Callable, sample: Callable, delta: Callable, log_handler: Callable
     ABC-MCMC approach is given below:
 
     1) We start with some initial guess theta_0. Right off the bat, we move to another theta from theta_0.
-    2) For 'iterations_bounds[1] - iterations_bounds[0]' iterations...
+    2) For 'boundaries[1] - boundaries[0]' iterations...
         a) For 'simulation_n' iterations...
             i) We simulate a population using the given theta.
             ii) For each observed frequency ... 'D'
@@ -37,26 +66,31 @@ def run(walk: Callable, sample: Callable, delta: Callable, log_handler: Callable
     from types import SimpleNamespace
     from numpy.random import uniform
     from datetime import datetime
+    from numpy import zeros, mean
 
     # Save our results according to the namespace below.
-    a_record = lambda a, b, c, d, e, f: SimpleNamespace(theta=a, time_r=b, waiting_time=c,
-                                                        p_proposed=d, expected_delta=e, proposed_time=f)
+    a_record = lambda a_1, b_1, c_1, d_1, e_1, f_1: SimpleNamespace(theta=a_1, time_r=b_1, waiting_time=c_1,
+                                                                    p_proposed=d_1, expected_delta=e_1,
+                                                                    proposed_time=f_1)
 
     # Seed our Markov chain with our initial guess.
     x = [a_record(theta_0, 0, 1, 1.0e-10, 1.0e-10, 0)]
 
     for i in range(boundaries[0] + 1, boundaries[1]):
-        # Walk from our previous state.
-        theta_proposed = walk(x[-1].theta)
+        theta_proposed = walk(x[-1].theta)  # Walk from our previous state.
 
-        # Populate our H, D, and O matrices.
-        hdo = generate_hdo(observed, boundaries[1] - boundaries[0], [theta_proposed.kappa, theta_proposed.omega])
-        hd_results = populate_hd(hdo, sample, delta, theta_proposed, epsilon)
+        # Prepare our H and D matrices.
+        h = zeros((boundaries[1] - boundaries[0], len(observed)), dtype='int8')
+        d = zeros((boundaries[1] - boundaries[0], len(observed)), dtype='float64')
+
+        # Populate D, then H.
+        populate_d(d, observed, sample, delta, theta_proposed, [theta_proposed.kappa, theta_proposed.omega])
+        _populate_h(h, d, epsilon)
 
         # Accept our proposal according to our alpha value.
-        p_proposed, p_k = likelihood_from_h(hd_results.h), x[-1].p_proposed
+        p_proposed, p_k = _likelihood_from_h(h), x[-1].p_proposed
         if p_proposed / p_k > uniform(0, 1):
-            x = x + [a_record(theta_proposed, datetime.now(), 1, p_proposed, hd_results.expected_delta, i)]
+            x = x + [a_record(theta_proposed, datetime.now(), 1, p_proposed, mean(d), i)]
 
         # Reject our proposal. We keep our current state and increment our waiting times.
         else:
