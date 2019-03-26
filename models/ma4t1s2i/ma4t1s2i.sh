b6e775e1e1bcb7300ba49c688ba0e9638b49e284
Glenn <gjgalvizo@gmail.com>
2019-03-26 13:57:30 -1000

    Added progress bar to MA4T1S2I script as well.

--- models/ma4t1s2i/ma4t1s2i.sh
+++ models/ma4t1s2i/ma4t1s2i.sh
@@ -7,6 +7,13 @@ if [[ "$#" -ne 1 ]] && [[ "$#" -ne 2 ]]; then
 fi
 SCRIPT_DIR=$(dirname "$0")
 
+i=1  # Setup our progress bar.
+DURATION=100
+already_done() { for ((done=0; done < $i; done++)); do printf "▉"; done }
+remaining() { for ((remain=$i; remain < ${DURATION}; remain++)); do printf " "; done }
+percentage() { printf "| %s%%" $(( ($i*100)/${DURATION}*100/100 )); }
+clean_line() { printf "\r"; }
+
 # Our criteria for the loci and sample IDs to use for this run of MCMC: The Colombian populace.
 sample_uids=(); sample_loci=()
 for r in $(sqlite3 ${2:-data/observed.db} "SELECT DISTINCT SAMPLE_UID, LOCUS \
@@ -18,18 +25,20 @@ for r in $(sqlite3 ${2:-data/observed.db} "SELECT DISTINCT SAMPLE_UID, LOCUS \
 done
 
 # Run once to seed our database. Must break into parts because GC is garbage ):<
+already_done; remaining; percentage
 python3 ${SCRIPT_DIR}/ma4t1s2i.py \
 	-mdb "$1" \
     -simulation_n 100 \
     -epsilon 0.55 \
-    -iterations_n 10000 \
-    -flush_n 5000 \
+    -iterations_n 1000 \
+    -flush_n 500 \
     -uid ${sample_uids} \
     -loci ${sample_loci} \
     -delta_f cosine \
     -n_b 10 -n_b_sigma 5.0 \
     -n_s1 10 -n_s1_sigma 5.0 \
     -n_s2 10 -n_s2_sigma 5.0 \
+    -n_e 100 -n_e_sigma 5.0 \
     -f_b 100 -f_b_sigma 5.0 \
     -f_s1 100 -f_s1_sigma 5.0 \
     -f_e 100 -f_e_sigma 5.0 \
@@ -38,16 +47,17 @@ python3 ${SCRIPT_DIR}/ma4t1s2i.py \
     -d 0.00255 -d_sigma 0.0 \
     -kappa 3 -kappa_sigma 0.0 \
     -omega 30 -omega_sigma 0.0
-echo "MCMC Progress [1/10]."
+clean_line
 
 # Repeat 9 more times.
 for i in {2..10}; do
+	already_done; remaining; percentage
     python3 ${SCRIPT_DIR}/ma4t1s2i.py \
 		-mdb "$1" \
 		-simulation_n 100 \
 		-epsilon 0.55 \
-		-iterations_n 10000 \
-		-flush_n 5000 \
+		-iterations_n 1000 \
+		-flush_n 500 \
 		-uid ${sample_uids} \
 		-loci ${sample_loci} \
 		-delta_f cosine \
@@ -62,7 +72,7 @@ for i in {2..10}; do
 		-d_sigma 0.0 \
 		-kappa_sigma 0.0 \
 		-omega_sigma 0.0
-    echo "MCMC Progress [$i/10]."
+    clean_line
 done
 
-echo "MCMC is finished!"
+printf "\nMCMC is finished!"
