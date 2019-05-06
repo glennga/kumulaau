db7710c3c444dfa45638500cf20868eb4c279438
glenn <glenn@plumeria.kona.local>
2019-05-06 07:01:24 +0000

    We must now specify starting omega and kappa in chained runs.

--- models/abc1t0s0i/abc1t0s0i.sh
+++ models/abc1t0s0i/abc1t0s0i.sh
@@ -12,19 +12,19 @@ clean_line() { printf "\r"; }
 # Run once to seed our database. Must break into parts because GC is garbage ):<
 already_done; remaining; percentage
 python3 ${SCRIPT_DIR}/abc1t0s0i.py \
-    -mdb "${MDB}" \
-    -observations "${OBSERVATIONS}" \
-    -simulation_n ${SIMULATION_N} \
-    -epsilon ${EPSILON} \
-    -summary ${SUMMARY[*]} \
-    -iterations_n ${ITERATIONS_N} \
-    -flush_n ${FLUSH_N} \
-    -n_start ${N_START} -n_sigma ${N_SIGMA} \
-    -f_start ${F_START} -f_sigma ${F_SIGMA} \
-    -c_start ${C_START} -c_sigma ${C_SIGMA} \
-    -d_start ${D_START} -d_sigma ${D_SIGMA} \
-    -kappa_start ${KAPPA_START} -kappa_sigma ${KAPPA_SIGMA} \
-    -omega_start ${OMEGA_START} -omega_sigma ${OMEGA_SIGMA}
+	-mdb "${MDB}" \
+	-observations "${OBSERVATIONS}" \
+	-simulation_n ${SIMULATION_N} \
+	-epsilon ${EPSILON} \
+	-summary ${SUMMARY[*]} \
+	-iterations_n ${ITERATIONS_N} \
+	-flush_n ${FLUSH_N} \
+	-n_start ${N_START} -n_sigma ${N_SIGMA} \
+	-f_start ${F_START} -f_sigma ${F_SIGMA} \
+	-c_start ${C_START} -c_sigma ${C_SIGMA} \
+	-d_start ${D_START} -d_sigma ${D_SIGMA} \
+	-kappa_start ${KAPPA_START} -kappa_sigma ${KAPPA_SIGMA} \
+	-omega_start ${OMEGA_START} -omega_sigma ${OMEGA_SIGMA}
 clean_line
 
 # Repeat MCMC_LINKS - 1 more times.
@@ -35,15 +35,15 @@ for ((i=2; i<${MCMC_LINKS}; i++)); do
         -observations "${OBSERVATIONS}" \
 		-simulation_n ${SIMULATION_N} \
 		-epsilon ${EPSILON} \
-    	-summary ${SUMMARY[*]} \
+		-summary ${SUMMARY[*]} \
 		-iterations_n ${ITERATIONS_N} \
 		-flush_n ${FLUSH_N} \
 		-n_sigma ${N_SIGMA} \
 		-f_sigma ${F_SIGMA} \
 		-c_sigma ${C_SIGMA} \
 		-d_sigma ${D_SIGMA} \
-		-kappa_sigma ${KAPPA_SIGMA} \
-		-omega_sigma ${OMEGA_SIGMA}
+		-kappa_start ${KAPPA_START} -kappa_sigma ${KAPPA_SIGMA} \
+		-omega_start ${OMEGA_START} -omega_sigma ${OMEGA_SIGMA}
     clean_line
 done
 
--- models/ele1t0s0i/ele1t0s0i.sh
+++ models/ele1t0s0i/ele1t0s0i.sh
@@ -12,40 +12,40 @@ clean_line() { printf "\r"; }
 # Run once to seed our database. Must break into parts because GC is garbage ):<
 already_done; remaining; percentage
 python3 ${SCRIPT_DIR}/ele1t0s0i.py \
-    -mdb "${MDB}" \
+	-mdb "${MDB}" \
 	-observations "${OBSERVATIONS}" \
 	-simulation_n ${SIMULATION_N} \
-    -r ${R} \
-    -bin_n ${BIN_N} \
-    -summary ${SUMMARY[*]} \
-    -iterations_n ${ITERATIONS_N} \
-    -flush_n ${FLUSH_N} \
-    -n_start ${N_START} -n_sigma ${N_SIGMA} \
-    -f_start ${F_START} -f_sigma ${F_SIGMA} \
-    -c_start ${C_START} -c_sigma ${C_SIGMA} \
-    -d_start ${D_START} -d_sigma ${D_SIGMA} \
-    -kappa_start ${KAPPA_START} -kappa_sigma ${KAPPA_SIGMA} \
-    -omega_start ${OMEGA_START} -omega_sigma ${OMEGA_SIGMA}
+	-r ${R} \
+	-bin_n ${BIN_N} \
+	-summary ${SUMMARY[*]} \
+	-iterations_n ${ITERATIONS_N} \
+	-flush_n ${FLUSH_N} \
+	-n_start ${N_START} -n_sigma ${N_SIGMA} \
+	-f_start ${F_START} -f_sigma ${F_SIGMA} \
+	-c_start ${C_START} -c_sigma ${C_SIGMA} \
+	-d_start ${D_START} -d_sigma ${D_SIGMA} \
+	-kappa_start ${KAPPA_START} -kappa_sigma ${KAPPA_SIGMA} \
+	-omega_start ${OMEGA_START} -omega_sigma ${OMEGA_SIGMA}
 clean_line
 
 # Repeat MCMC_LINKS - 1 more times.
 for ((i=2; i<${MCMC_LINKS}; i++)); do
     already_done; remaining; percentage
     python3 ${SCRIPT_DIR}/ele1t0s0i.py \
-        -mdb "${MDB}" \
-        -observations "${OBSERVATIONS}" \
+		-mdb "${MDB}" \
+		-observations "${OBSERVATIONS}" \
 		-simulation_n ${SIMULATION_N} \
-    	-r ${R} \
-    	-bin_n ${BIN_N} \
-    	-summary ${SUMMARY[*]} \
+		-r ${R} \
+		-bin_n ${BIN_N} \
+		-summary ${SUMMARY[*]} \
 		-iterations_n ${ITERATIONS_N} \
 		-flush_n ${FLUSH_N} \
 		-n_sigma ${N_SIGMA} \
 		-f_sigma ${F_SIGMA} \
 		-c_sigma ${C_SIGMA} \
 		-d_sigma ${D_SIGMA} \
-		-kappa_sigma ${KAPPA_SIGMA} \
-		-omega_sigma ${OMEGA_SIGMA}
+		-kappa_start ${KAPPA_START} -kappa_sigma ${KAPPA_SIGMA} \
+		-omega_start ${OMEGA_START} -omega_sigma ${OMEGA_SIGMA}
     clean_line
 done
 
